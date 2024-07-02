e7915552a53a8cc3c20aa8893c3156b787f10041
haisombo <haisombo20@gmail.com>
2024-07-02 10:26:52 +0700

    Initial Commit

---
+++ DataAccessDynamic/DataAccessDynamic.h
@@ -0,0 +1,18 @@
+//
+//  DataAccessDynamic.h
+//  DataAccessDynamic
+//
+//  Created by Hai Sombo on 7/2/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataAccessDynamic.
+FOUNDATION_EXPORT double DataAccessDynamicVersionNumber;
+
+//! Project version string for DataAccessDynamic.
+FOUNDATION_EXPORT const unsigned char DataAccessDynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataAccessDynamic/PublicHeader.h>
+
+
